7752c2385afa47aaa73c1c0bcb28010a79890d87
Peter <peter.mccurrach@gmail.com>
2014-09-05 16:44:51 +0100

    json parser

--- hello.rb
+++ hello.rb
@@ -1,7 +1,11 @@
 require 'rubygems'
 require 'sinatra'
+require 'json'
 
 post '/update' do
 	data = JSON.parse(request.body.string)
-	
+	result = JSON.pretty_generate(data)
+# 	result = "HELLO = #" + data.keys.join(", ") + "#" + data["hello"]
+#	why doesn't data[:hello] work but data["hello"] does???
+	result
 end
